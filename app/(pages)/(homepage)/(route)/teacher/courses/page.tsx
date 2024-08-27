8ad80bafdc35c2e8d27dbf36bfd65dfe9d4f033f
Akinkuowo <akinseun1990@gmail.com>
2024-08-28 00:27:12 +0100

    updating app

--- app/(pages)/(homepage)/(route)/teacher/courses/page.tsx
+++ app/(pages)/(homepage)/(route)/teacher/courses/page.tsx
@@ -1,14 +1,38 @@
+import { auth } from "@clerk/nextjs/server";
 import { Button } from "@/components/ui/button";
+import { DataTable } from "./_components/data-table";
 import Link from "next/link";
+import { columns } from "./_components/columns";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { PlusCircle } from "lucide-react";
 
-const TeacherPage = () => {
+
+const TeacherPage = async () => {
+   const { userId } = auth()
+
+   if(!userId){
+     return redirect("/")
+   }
+
+   const courses = await db.course.findMany({
+        where: {
+            userId    
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+   })
+
     return ( 
         <div className="p-6">
             <Link href="/teacher/courses/create">
                 <Button>
+                    <PlusCircle className="h-4 w-4 mr-2"/>
                     New Course
                 </Button>
             </Link>
+            <DataTable columns={columns} data={courses} />
         </div>
      );
 }
